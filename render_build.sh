691f7230915fd598175e3c80df1cc0281d0e30c0
David Betancourt <dmarquez.develop@gmail.com>
2025-10-05 21:52:00 +0000

    added pip files to render build before render

--- render_build.sh
+++ render_build.sh
@@ -8,3 +8,17 @@ npm run build
 pip install
 
 pip run upgrade
+
+pip install --upgrade pip
+pip install psycopg2-binary==2.9.9
+pip install flask==2.3.3
+pip install flask-sqlalchemy==3.0.5
+pip install flask-migrate==4.0.5
+pip install flask-cors==4.0.0
+pip install flask-swagger==0.2.14
+pip install flask-admin==1.6.1
+pip install gunicorn==21.2.0
+pip install python-dotenv==1.0.0
+pip install alembic==1.12.1
+pip install pyyaml==6.0.1
+flask db upgrade
